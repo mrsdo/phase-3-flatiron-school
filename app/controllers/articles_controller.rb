0cc02748de5e44289e7495a0e8107d61dc309dfc
Marti D <me@martidolce.com>
2021-02-10 23:38:17 -0500

    Adding update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,12 @@ class ArticlesController < ApplicationController
 
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    # @article = Article.new(params.require(:article).permit(:title, :text))
     @article = Article.new(article_params)
     # render plain: params[:article].inspect
     if @article.save
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
       render "articles/new"
     end
     end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if@article.update(article_params)
+      redirect_to@article
+    else
+      render "articles/edit"
+    end
+  end
 
   def show
     @article = Article.find(params[:id])
